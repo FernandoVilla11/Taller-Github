278f2c6ee122d5593039276e7cf3f35bca6de859
Fernando Villadiego <jose.villadiego096@gmail.com>
2024-05-08 22:55:14 -0500

    Adding video-views structures

---
+++ src/main/java/exception/VideoNotFouundException.java
@@ -0,0 +1,15 @@
+package exception;
+
+
+import java.text.MessageFormat;
+public class VideoNotFouundException extends CulturemediaException{
+
+    public VideoNotFoundException() {
+        super("Video not found");
+    }
+
+    public VideoNotFoundException(String title) {
+        super(MessageFormat.format("Invalid title for the video {0}", title));
+    }
+
+}
